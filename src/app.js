617c8c900f025bc3e566468c6874fd30faec58be
kazs693 <karenesearle@gmail.com>
2025-10-10 16:24:52 +1100

    Added additional weather data

--- src/app.js
+++ src/app.js
@@ -4,6 +4,40 @@ function refreshWeather(response) {
 
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
+
+  let dateElement = document.querySelector("#date-time");
+  let date = new Date(response.data.time * 1000);
+  dateElement.innerHTML = formatDate(date);
+
+  let conditionElement = document.querySelector("#condition");
+  conditionElement.innerHTML = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
