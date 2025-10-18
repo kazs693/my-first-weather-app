589033043317676a6cd8aff0fd19afd051848bba
kazs693 <karenesearle@gmail.com>
2025-10-18 13:16:57 +1100

    Updated to Inject forecast html from JavaScript

--- src/app.js
+++ src/app.js
@@ -60,7 +60,31 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">⛅</div>
+    <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+        <strong>15°</strong>
+      </div>
+      <div class="weather-forecast-temperature">9°</div>
+    </div>
+  </div>`;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Sydney"); //Default city for initial page load
+
+displayForecast();
